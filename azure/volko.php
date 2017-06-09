10f9772f39822efc1a41399fb3e93f270bd40957
MATIAS EZEQUIEL ZELAZNY <42102578@ort>
2017-06-09 11:57:51 -0300

    voas,lfdkjasdjkas

--- azure/volko.php
+++ azure/volko.php
@@ -1,5 +1,9 @@
 <?php
+$link = mysql_connect('127.0.0.1:56915', 'azure', '6#vWHD_$')
+    or die('No se pudo conectar: ' . mysql_error());
+echo 'Connected successfully';
+mysql_select_db('localdb') or die('No se pudo seleccionar la base de datos');
 
-echo 'gato';
+echo 'volko gato';
 
 ?>
