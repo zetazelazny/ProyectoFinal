4d0ac1c4f37165635bd5e670d2b0b02a1e0e4dff
MATIAS EZEQUIEL ZELAZNY <42102578@ort>
2017-06-16 08:39:46 -0300

    caro

--- azure/volko.php
+++ azure/volko.php
@@ -2,9 +2,7 @@
 $link = mysql_connect('127.0.0.1:56915', 'azure', '6#vWHD_$') or die('No se pudo conectar: ' . mysql_error());
 mysql_select_db('localdb') or die('No se pudo seleccionar la base de datos');
 $result = mysql_query("SELECT nombre FROM usuarios", $link); 
-/*while ($row = mysql_fetch_row($result)){ 
+while ($row = mysql_fetch_row($result)){ 
 echo $row['nombre'];
-*/
-echo 'aksdasjokdas';
 }
 ?>
