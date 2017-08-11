8e38a9f124a6b4f6b8f677a173855dabb7358e3f
zetazelazny <matiaszelazny99@gmail.com>
2017-08-11 10:04:37 -0300

    Hasta la victoria siempre, y comer cremona

--- Integrapp/app/src/main/java/com/example/a42102578/integrapp/Memotest.java
+++ Integrapp/app/src/main/java/com/example/a42102578/integrapp/Memotest.java
@@ -2,9 +2,11 @@ package com.example.a42102578.integrapp;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -18,8 +20,16 @@ import java.sql.Statement;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Memotest extends AppCompatActivity {
+    public Boolean listoParaAgregar = false;
     int[] VectorRandom = new int[16];
     boolean[] VectorEstado = new boolean[16];
     ImageButton[] VectorBotones = new ImageButton[16];
@@ -975,10 +985,86 @@ public class Memotest extends AppCompatActivity {
         }
         if (Gano)
         {
-            Intent Intento = new Intent(this, Gano.class);
-            startActivity(Intento);
-            finish();
+            listoParaAgregar = false;
+            enviarPuntaje(new String[]{"Memotest" ,String.valueOf(ContPuntos)});
         }
 
+    }
+
+    private void enviarPuntaje(String[] params){
+        new enviarPuntos().execute(params[0], params[1]);
+    }
+
+    private class enviarPuntos extends AsyncTask<String, Void, String> {
+
+        protected void onPostExecute(String datos) {
+            super.onPostExecute(datos);
+
+            if (datos.equals("error")) {
+                //Toast.makeText(getActivity(), "Comprueba tu conexión a Internet", Toast.LENGTH_SHORT).show();
+
+            } else if (datos.equals("error2")) {
+                // El token está mal, asi que a borrarloo y que vuelva al inicio
+                //Toast.makeText(getActivity(), "Sesión expirada, vuelve a iniciar sesion.", Toast.LENGTH_SHORT).show();
+            } else if (datos.equals("faltanDatos")) {
+
+            } else {
+                //ok
+            }
+
+            if (listoParaAgregar) {
+                Intent Gano = new Intent(getApplicationContext(), Gano.class);
+                startActivity(Gano);
+                finish();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... parametros) {
+            Log.d("Juego", "1");
+            if (new HttpHandler(getApplicationContext()).hasActiveInternetConnection()) {
+                OkHttpClient client = new OkHttpClient();
+                client.newBuilder().connectTimeout(10, TimeUnit.SECONDS)
+                        .readTimeout(10, TimeUnit.SECONDS).build();
+                Log.d("Juego", "2");
+                if (parametros[0].trim().length() > 0) {
+                    Log.d("Juego", "3");
+
+                    RequestBody requestBody = new MultipartBody.Builder()
+                            .setType(MultipartBody.FORM)
+                            .addFormDataPart("juego", parametros[0])
+                            .addFormDataPart("puntaje", parametros[1])
+                            .build();
+                    Log.d("Juego", "4");
+
+                    listoParaAgregar = true;
+
+                    Log.d("Juego", "5");
+
+                    Request request = new Request.Builder()
+                            .url("http://integrapp.azurewebsites.net/azure/insertpuntos.php")
+                            .method("POST", RequestBody.create(null, new byte[0]))
+                            .post(requestBody)
+                            .build();
+
+                    Log.d("Juego", "6");
+
+                    try {
+                        Response response = client.newCall(request).execute();
+                        String resultado = response.body().string();
+                        Log.d("Juego", "7");
+                        return resultado;
+                    } catch (Exception e) {
+                        Log.d("Debug", e.getMessage());
+                        //mostrarError(e.getMessage()); // Error de Network
+                        return "error";
+                    }
+                } else {
+                    return "faltanDatos";
+                }
+            } else {
+                return "error";
+            }
+        }
     }
 }
