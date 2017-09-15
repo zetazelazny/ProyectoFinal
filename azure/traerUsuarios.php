bb8345d1f9f0d695cc622bf0fdd8ff7226827afc
MATIAS EZEQUIEL ZELAZNY <42102578@ort>
2017-09-15 09:48:52 -0300

    GG

--- azure/traerUsuarios.php
+++ azure/traerUsuarios.php
@@ -1,7 +1,8 @@
 <?php
 $conexion = new PDO('mysql:host=127.0.0.1:56915;dbname=integrapp;charset=utf8mb4', 'azure', '6#vWHD_$');
 $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$stmt = $conexion->prepare("SELECT id, nombre, apellido FROM pacientes ");
+$id_profesional = $_POST['id_profesional'];
+$stmt = $conexion->prepare("SELECT id, nombre, apellido FROM pacientes WHERE '".$id_profesional."' = id_profesional");
 $stmt->execute();
 $var = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $json = json_encode($var);
